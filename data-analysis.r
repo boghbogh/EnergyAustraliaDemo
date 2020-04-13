56c911cd0e38586ccc61c32d2abf21bd9579d047
boghbogh <>
2020-04-13 05:14:21 +0000

    Adding few sql statement

--- data-analysis.r
+++ data-analysis.r
@@ -1,2 +1,18 @@
 # Databricks notebook source
-# MAGIC %fs ls
+# MAGIC %sql
+# MAGIC create database EADemo;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use  EADemo;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from iou
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from nou
